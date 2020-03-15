0c251b5b0335ad0b1fc104e90573e0df3a62b400
efcjunior <everson.c.junior@gmail.com>
2020-03-15 18:19:12 -0300

    test

--- lesson-02/cat-clicker/js/app.js
+++ lesson-02/cat-clicker/js/app.js
@@ -40,6 +40,8 @@
             let itemsChanged = this.getAllItems()
             itemsChanged.map(iItem =>{
                 if(iItem.id == item.id){
+                    iItem.catName = item.catName
+                    iItem.pictureFileName = item.pictureFileName
                     iItem.clickCount = item.clickCount
                 }
             })
@@ -52,6 +54,7 @@ var octopus = {
         model.init()
         itemsView.init()
         itemView.init()
+        adminAreaView.init()
     },
 
     getCurrentItem: function(){
@@ -72,6 +75,23 @@ var octopus = {
         model.currentItem.clickCount++
         model.updateItem(model.currentItem)
         itemView.render()
+    },
+
+    changeCurrentItem: function(newItem){
+        let item = {}
+        newItem.forEach(e => {
+            item[e.name] = e.value
+        })
+        item.pictureFileName = model.currentItem.pictureFileName
+        item.id = model.currentItem.id
+        this.updateCurrentItem(item)
+    },
+
+    updateCurrentItem: function(newItem){
+        model.currentItem = newItem
+        model.updateItem(model.currentItem)
+        itemsView.render()
+        itemView.render()        
     }
 }
 
@@ -83,12 +103,14 @@ var itemsView = {
     },
 
     render: function(){      
+        this.items.empty()
         octopus.getItems().forEach(item => {
             let itemLi = document.createElement("li");
             itemLi.innerText = item.catName;
             itemLi.classList.add('item')
             itemLi.addEventListener('click', (function(cat){
                 return function () {
+                    adminAreaView.render()
                     octopus.setCurrentItem(cat)
                     itemView.render()
                 }
@@ -113,6 +135,33 @@ var itemView = {
         this.catName.text(cat.catName)
         this.clickCount.text(cat.clickCount)
         this.catPicture.attr('src' , `images/${cat.pictureFileName}`)
+    }
+}
+
+var adminAreaView = {
+
+    init: function(){
+        this.adminForm = $('.box-admin-form form')
+        this.adminForm.on('submit', this.submitNewData)
+        this.adminButton = $('#btn-admin')        
+        this.adminButton.on('click',this.render.bind(this))     
+    },
+
+    render: function(event){
+        const adminArea = $('.box-admin-form')                 
+        if(event && adminArea.css('display') == 'none'){
+            adminArea.css('display','inline-block')
+            this.adminForm[0].reset()
+            this.adminForm.find(':input[name=pictureFileName]')
+                .val(octopus.getCurrentItem().pictureFileName)
+        }else{
+            adminArea.css('display','none')
+        }
+    },
+
+    submitNewData : function(event){
+        event.preventDefault()
+        octopus.changeCurrentItem($(this).serializeArray())
     }
 }
 
